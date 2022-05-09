c90c607f54839d911cef4df9f27c7128d768847f
dyuupp <rizosh@yandex.ru>
2022-05-09 18:56:38 +0300

    added .gitignore

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 5000;
+        int bonus = 20;
+        int total = price / bonus;
+        System.out.println(total);
+        }
+    }
+
+
+
+
